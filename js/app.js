7b23b8f03dc3b5a2af8d956900f687f4ce53154e
yun <graypalett@naver.com>
2021-12-08 16:48:39 +0900

    swipe complete

--- js/app.js
+++ js/app.js
@@ -2,83 +2,37 @@
 
 // index 슬라이드 부분
 
-var banner_left = 0;
-var img_cnt = 0;
-var first = 1;
-var last;
-var interval;
-
-$(document).ready(function () {
-  $(".slide_in").each(function () {
-    $(this).css("left", banner_left);
-    banner_left += $(this).width() + 5;
-    $(this).attr("id", "content" + ++img_cnt);
-  });
-
-  last = img_cnt;
-  startAction();
-
-  $(".slide_in").hover(
-    function () {
-      stopAction();
-    },
-    function () {
-      startAction();
-    }
-  );
-});
-
-function startAction() {
-  interval = setInterval(function () {
-    $(".slide_in").each(function () {
-      $(this).css("left", $(this).position().left - 1);
-    });
-
-    var first_content = $("#content" + first);
-    var last_content = $("#content" + last);
-
-    if (first_content.position().left < "-" + $(first_content).width()) {
-      first_content.css(
-        "left",
-        last_content.position().left + last_content.width() + 5
-      );
-      first++;
-      last++;
-      if (last > img_cnt) {
-        last = 1;
-      }
-      if (first > img_cnt) {
-        first = 1;
-      }
-    }
-  }, 15);
-}
-
-function stopAction() {
-  clearInterval(interval);
-}
-
 // swiper
 
 $(document).ready(function () {
   var mySwiper = new Swiper(".swiper-container", {
     direction: "horizontal",
     loop: true,
+    slidesPerView: 5.5,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 3000, // 시간 설정
+    },
+    // Responisve breakpoint
     breakpoints: {
       // when window width is >= 320px
       320: {
-        slidesPerView: 2,
-        spaceBetween: 20,
+        slidesPerView: 1.5,
+        spaceBetween: 4,
       },
       // when window width is >= 480px
       480: {
-        slidesPerView: 3,
-        spaceBetween: 30,
+        slidesPerView: 2,
+        spaceBetween: 4,
       },
       // when window width is >= 640px
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 40,
+      768: {
+        slidesPerView: 2.5,
+        spaceBetween: 8,
+      },
+      1300: {
+        slidesPerView: 3.5,
+        spaceBetween: 10,
       },
     },
   });
