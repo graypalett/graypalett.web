934283ada6c8fcaa1f8ecb45a4c845be9a7682f8
yun <graypalett@naver.com>
2021-12-08 15:30:59 +0900

    swiper

--- js/app.js
+++ js/app.js
@@ -57,3 +57,29 @@ function startAction() {
 function stopAction() {
   clearInterval(interval);
 }
+
+// swiper
+
+$(document).ready(function () {
+  var mySwiper = new Swiper(".swiper-container", {
+    direction: "horizontal",
+    loop: true,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+  });
+});
